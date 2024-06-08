f1775fd945ac6c13ddfd9b8654d10ca90d430283
Tech Azonic <59231342+sarubasant@users.noreply.github.com>
2024-06-08 09:49:50 +0000

    files

--- app.js
+++ app.js
@@ -5,4 +5,17 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/about', function (req, res) {
+  res.send('About page hey ye')
+})
+
+app.get('/profile', function (req, res,next) {
+  return next(new Error("Console: Something went wrong"));
+})
+
+app.use((err,req,res,next)=>{
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 app.listen(3000)
