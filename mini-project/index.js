0570f36d68ab496064475f68e598973da4a90684
Tech Azonic <59231342+sarubasant@users.noreply.github.com>
2024-06-09 05:48:48 +0000

    practice todo

--- mini-project/index.js
+++ mini-project/index.js
@@ -1,16 +1,26 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 const path = require('path');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname),'public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
     res.render("index");
 });
 
-app.listen(3000,function(){
+//dynamic route
+app.get("/profile/:username", function (req, res) {
+    const user = req.params.username;
+    res.send('welcome ' + user);
+});
+app.get("/author/:username/:age", function (req, res) {
+    const user = req.params.username;
+    res.send(`welcome ${user} of age ${req.params.age}`);
+});
+
+app.listen(3000, function () {
     console.log("its runnning");
 })
